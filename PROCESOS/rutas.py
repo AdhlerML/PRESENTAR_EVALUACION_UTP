6079bfe96d5344ed3120b6464d9bb72a7ab32ae3
ADHLER <adhler_25@hotmail.com>
2025-09-10 00:08:10 -0500

    segundo

--- PROCESOS/rutas.py
+++ PROCESOS/rutas.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 def obtener_rutas():
-    ROOT = Path(__file__).resolve().parents[1]
+    ROOT = Path(__file__).resolve().parents[2]
     in_file = ROOT / "PRESENTAR_EVALUACION_UTP" / "DATOS" / "DIRTY" / "sucio.csv"
     out_file = ROOT / "PRESENTAR_EVALUACION_UTP" / "DATOS" / "CLEAR" / "limpio.csv"
     return in_file, out_file
